f66b0fe84f627acb2eadd2d99c28116b465efda6
bernazsn <nogueirab468@gmail.com>
2026-03-09 09:15:35 -0300

    Add files via upload
    Numero positivo ou negativo.

---
+++ python.py
@@ -0,0 +1,8 @@
+numero = float(input('Digite um numero:'))
+if numero>0:
+        print(f"0 numero {numero} é positivo.")
+else:
+    if numero <0:
+        print(f"0 numero {numero} é negativo.")
+    else:
+        print("o numero é zero.")
